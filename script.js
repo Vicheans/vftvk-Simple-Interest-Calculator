fa5406dedcf8912ad3cdefb1f71e5698e5c1efc6
Olateju Victor Daniel <christianovik009@gmail.com>
2021-11-13 23:40:47 +0100

    Final Project Vicheans

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+window.onload = function () {
+  document.getElementById("rate").oninput = function () {
+    let value = (
+      ((this.value - this.min) / (this.max - this.min)) *
+      100
+    ).toFixed(2);
+    this.style.background =
+      "linear-gradient(to right, #0056d2 0%, #0056d2 " +
+      value +
+      "%, #fff " +
+      value +
+      "%, white 100%)";
+    document.getElementById("rate_val").textContent = this.value + "%";
+  };
+  compute(); 
+};
+
+
+  function compute() {
+    const principal = document.getElementById("principal").value;
+    const rate = document.getElementById("rate").value;
+    const time = document.getElementById("years").value;
+
+    const result = (principal * rate * time) / 100;
+
+    const output =
+      `<p> If you deposit <span class='tagged'>${principal}</span>, <br />` +
+      `at an interest rate of <span class='tagged'>${rate}%</span>. <br />` +
+      `You will receive an amount of <span class='tagged'>${result}</span>, <br />` +
+      `in the year <span class='tagged'>${
+        new Date().getFullYear() + +time
+      }</span> </p>`;
+
+    document.getElementById("result").innerHTML = output;
+  }
